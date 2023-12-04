2c93c1971f09ca3c3c73175b751255eea3d7d2b4
Rafael Magno <rafaelmagno125@yahoo.com.br>
2023-12-04 05:58:00 -0300

    removendo arquivo desnecessário

--- src/public/js/video.js
+++
@@ -1,9 +0,0 @@
-export class Video {
-  ocultarControles(id) {
-    const video = document.getElementById(id);
-
-    video.addEventListener("ended", () => {
-      video.removeAttribute("controls");
-    });
-  }
-}
